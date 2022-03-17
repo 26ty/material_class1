a5403826bd9a143907aae4e1e963315e753fef47
NX <ch1342213422@gmail.com>
2022-03-18 03:05:39 +0800

    feat: 新增導覽列與成績登錄頁面

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  gradeForm:number = 5
+  addFormBtnSta:boolean = false
+  ngOnInit(): void {
+
+  }
+
+  addFormBtn(){
+    // this.addFormBtnSta = !this.addFormBtnSta
+    this.gradeForm ++
+    console.log(this.gradeForm)
+  }
+
+}
